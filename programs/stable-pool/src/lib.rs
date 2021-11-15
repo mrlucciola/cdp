4623feeafd49549dcfea672bd6150f706042e1dd
user5 <anon_user5@email.com>
2021-11-15 16:03:19 +0300

    add/repay usd & processors

--- programs/stable-pool/src/lib.rs
+++ programs/stable-pool/src/lib.rs
@@ -23,7 +23,6 @@ use crate::{
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
-
 #[program]
 pub mod stable_pool {
     use super::*;
@@ -31,7 +30,11 @@ pub mod stable_pool {
     pub fn create_global_state(ctx: Context<CreateGlobalState>, nonce: u8) -> ProgramResult { process_create_global_state(ctx, nonce) }
     pub fn create_token_vault(ctx: Context<CreateTokenVault>, nonce: u8) -> ProgramResult { process_create_token_vault(ctx, nonce)}
     pub fn create_user_trove(ctx: Context<CreateUserTrove>, nonce: u8) -> ProgramResult { process_create_user_trove(ctx, nonce) }
-    pub fn deposit_collateral(ctx: Context<DepositCollateral>, amount: u64) -> ProgramResult { process_deposit_collateral(ctx, amount) }
-    pub fn mint_usdx(ctx: Context<MintUSDx>, amount: u64) -> ProgramResult { process_mint_usdx(ctx, amount) }
 
+    pub fn deposit_collateral(ctx: Context<DepositCollateral>, amount: u64) -> ProgramResult { process_deposit_collateral(ctx, amount) }
+    pub fn withdraw_collateral(ctx: Context<WithdrawCollateral>, amount: u64) -> ProgramResult { process_withdraw_collateral(ctx, amount) }
+    pub fn repay_collateral(ctx: Context<RepayCollateral>, amount: u64) -> ProgramResult { process_repay_collateral(ctx, amount) }
+
+    pub fn borrow_usd(ctx: Context<BorrowUsd>, amount: u64) -> ProgramResult { process_borrow_usd(ctx, amount) }
+    pub fn repay_usd(ctx: Context<RepayUsd>, amount: u64) -> ProgramResult { process_repay_usd(ctx, amount) }
 }
