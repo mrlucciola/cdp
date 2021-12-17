33b81b2591c1a0436a3e73ce42f97a0925c8db38
user5 <anon_user5@email.com>
2021-12-17 18:22:14 +0300

    update

--- analyze/analyze-users.ts
+++ analyze/analyze-users.ts
@@ -1,14 +1,15 @@
 import * as anchor from '@project-serum/anchor';
 
-const LIMIT = 1000
+const LIMIT = 100
 
 export async function analyzeFaucet(connection: anchor.web3.Connection, programId: anchor.web3.PublicKey) {
     let result:any = {}
     const limit = LIMIT
     let confirmedSigInfos: anchor.web3.ConfirmedSignatureInfo[] = []
     let tempInfos = []
+    let start = 1
     do {
-        console.log('getting transactions from ' + confirmedSigInfos.length + ' ...')
+        console.log('getting transactions from ' + start + ' ...')
         if(confirmedSigInfos.length > 0){
             const before = confirmedSigInfos[confirmedSigInfos.length-1].signature
             tempInfos = await connection.getConfirmedSignaturesForAddress2(programId, {limit: limit, before: before}, 'finalized')
@@ -16,29 +17,31 @@ export async function analyzeFaucet(connection: anchor.web3.Connection, programI
         else {
             tempInfos = await connection.getConfirmedSignaturesForAddress2(programId, {limit:limit}, 'finalized')
         }
-        confirmedSigInfos = [...confirmedSigInfos, ...tempInfos]
+        confirmedSigInfos = tempInfos
+        start += confirmedSigInfos.length
+        
+        const signatures = []
+
+        confirmedSigInfos.forEach((info) => {
+            signatures.push(info.signature)
+        })
+        
+        const parsedConfirmedTxs = await connection.getParsedConfirmedTransactions(signatures, 'finalized')
+        parsedConfirmedTxs.forEach((parsedConfirmedTx) => {
+            try{
+                const owner = (parsedConfirmedTx.meta.postTokenBalances[0] as any).owner;
+                const mint = (parsedConfirmedTx.meta.postTokenBalances[0] as any).mint;
+                if(result[owner] === undefined) {
+                    result[owner] = {}
+                }
+                result[owner][mint] = result[owner][mint] ? result[owner][mint] + 1 : 1
+            }
+            catch(e){}
+        })
     }
     while(tempInfos.length >= limit);
-    console.log('confirmed faucet signatures count', confirmedSigInfos.length)
 
-    const signatures = []
-
-    confirmedSigInfos.forEach((info) => {
-        signatures.push(info.signature)
-    })
     
-    const parsedConfirmedTxs = await connection.getParsedConfirmedTransactions(signatures, 'finalized')
-    parsedConfirmedTxs.forEach((parsedConfirmedTx) => {
-        try{
-            const owner = (parsedConfirmedTx.meta.postTokenBalances[0] as any).owner;
-            const mint = (parsedConfirmedTx.meta.postTokenBalances[0] as any).mint;
-            if(result[owner] === undefined) {
-                result[owner] = {}
-            }
-            result[owner][mint] = result[owner][mint] ? result[owner][mint] + 1 : 1
-        }
-        catch(e){}
-    })
     return result
 }
 
@@ -47,8 +50,9 @@ export async function analyzePool(connection: anchor.web3.Connection, programId:
     const limit = LIMIT
     let confirmedSigInfos: anchor.web3.ConfirmedSignatureInfo[] = []
     let tempInfos = []
+    let start = 1
     do {
-        console.log('getting transactions from ' + confirmedSigInfos.length + ' ...')
+        console.log('getting transactions from ' + start + ' ...')
         if(confirmedSigInfos.length > 0){
             const before = confirmedSigInfos[confirmedSigInfos.length-1].signature
             tempInfos = await connection.getConfirmedSignaturesForAddress2(programId, {limit: limit, before: before}, 'finalized')
@@ -56,52 +60,54 @@ export async function analyzePool(connection: anchor.web3.Connection, programId:
         else {
             tempInfos = await connection.getConfirmedSignaturesForAddress2(programId, {limit:limit}, 'finalized')
         }
-        confirmedSigInfos = [...confirmedSigInfos, ...tempInfos]
-    }
-    while(tempInfos.length >= limit);
-    console.log('confirmed pool signatures count', confirmedSigInfos.length)
+        confirmedSigInfos = tempInfos
+        start += confirmedSigInfos.length
 
-    const signatures = []
+        const signatures = []
 
-    confirmedSigInfos.forEach((info) => {
-        signatures.push(info.signature)
-    })
-    
-    const parsedConfirmedTxs = await connection.getParsedConfirmedTransactions(signatures, 'finalized')
-    parsedConfirmedTxs.forEach((parsedConfirmedTx) => {
-        try{
-            if(parsedConfirmedTx.meta.postTokenBalances.length > 0) {
-                for(let i=0;i<parsedConfirmedTx.meta.postTokenBalances.length;i++){
-                    const owner = (parsedConfirmedTx.meta.postTokenBalances[i] as any).owner;
-                    const mint = (parsedConfirmedTx.meta.postTokenBalances[i] as any).mint;
-                    const amount = parsedConfirmedTx.meta.preTokenBalances[i].uiTokenAmount.uiAmount - parsedConfirmedTx.meta.postTokenBalances[i].uiTokenAmount.uiAmount
-                    if(result[owner] === undefined) {
-                        result[owner] = {}
-                    }
-                    if(result[owner][mint] === undefined) {
-                        result[owner][mint] = {
-                            'decreaseCount': 0,
-                            'decreaseAmount': 0,
-                            'increaseCount': 0,
-                            'increaseAmount': 0,
-                            'zeroCount': 0,
+        confirmedSigInfos.forEach((info) => {
+            signatures.push(info.signature)
+        })
+        
+        const parsedConfirmedTxs = await connection.getParsedConfirmedTransactions(signatures, 'finalized')
+        parsedConfirmedTxs.forEach((parsedConfirmedTx) => {
+            try{
+                if(parsedConfirmedTx.meta.postTokenBalances.length > 0) {
+                    for(let i=0;i<parsedConfirmedTx.meta.postTokenBalances.length;i++){
+                        const owner = (parsedConfirmedTx.meta.postTokenBalances[i] as any).owner;
+                        const mint = (parsedConfirmedTx.meta.postTokenBalances[i] as any).mint;
+                        const amount = parsedConfirmedTx.meta.preTokenBalances[i].uiTokenAmount.uiAmount - parsedConfirmedTx.meta.postTokenBalances[i].uiTokenAmount.uiAmount
+                        if(result[owner] === undefined) {
+                            result[owner] = {}
                         }
-                    }
-                    if(amount < 0){
-                        result[owner][mint]['decreaseCount'] += 1
-                        result[owner][mint]['decreaseAmount'] += amount
-                    }
-                    else if (amount > 0){
-                        result[owner][mint]['increaseCount'] += 1
-                        result[owner][mint]['increaseAmount'] += amount
-                    }
-                    else {
-                        result[owner][mint]['zeroCount'] += 1
+                        if(result[owner][mint] === undefined) {
+                            result[owner][mint] = {
+                                'decreaseCount': 0,
+                                'decreaseAmount': 0,
+                                'increaseCount': 0,
+                                'increaseAmount': 0,
+                                'zeroCount': 0,
+                            }
+                        }
+                        if(amount < 0){
+                            result[owner][mint]['decreaseCount'] += 1
+                            result[owner][mint]['decreaseAmount'] += amount
+                        }
+                        else if (amount > 0){
+                            result[owner][mint]['increaseCount'] += 1
+                            result[owner][mint]['increaseAmount'] += amount
+                        }
+                        else {
+                            result[owner][mint]['zeroCount'] += 1
+                        }
                     }
                 }
             }
-        }
-        catch(e){}
-    })
+            catch(e){}
+        })
+    }
+    while(tempInfos.length >= limit);
+
+    
     return result
 }
