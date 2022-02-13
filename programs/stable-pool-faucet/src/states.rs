01121ecb39ee5ae34562be9cec3d465aaf6ee5c1
user5 <anon_user5@email.com>
2022-02-14 02:45:24 +0300

    remove/pyth, utils

--- programs/stable-pool-faucet/src/states.rs
+++ programs/stable-pool-faucet/src/states.rs
@@ -5,7 +5,11 @@ use anchor_lang::prelude::*;
 pub struct Faucet {
     pub super_owner: Pubkey,
     pub mint_usdc_usdx_lp: Pubkey,
+    // pub mint_wtust_usdc_lp: Pubkey,
     pub mint_eth_sol_lp: Pubkey,
+    // pub mint_usdc_usdt_lp: Pubkey,
     pub mint_atlas_ray_lp: Pubkey,
+    // pub mint_ust_3pool_lp: Pubkey,
     pub mint_samo_ray_lp: Pubkey,
+    // pub mint_usdc_cash_lp: Pubkey,
 }
